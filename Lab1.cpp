0326293f6762f1cafc3b345951d74fb25c167728
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-03-15 13:20:27 +0300

    Create Lab1.cpp

---
+++ Lab1.cpp
@@ -0,0 +1,23 @@
+//lab1.1
+#include <iostream>
+using namespace std;
+int main() {
+	float gallons, cufeet;
+	cout << "Enter quantity in gallons" << endl;
+	cin >> gallons;
+	cufeet = gallons / 7.481;
+	cout << "Equivalent in cubic feet is " << cufeet << endl;
+	return 0;
+}
+
+//lab1.2
+//lab1.3
+//lab1.4
+//lab1.5
+//lab1.6
+//lab1.7
+//lab1.8
+//lab1.9
+//lab1.10
+//lab1.11
+//lab1.12
