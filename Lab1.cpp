b2a17df6a2b91118863573941830db447589f2e6
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-03-16 20:32:09 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -95,7 +95,17 @@ int main() {
 }
 
 //lab1.8
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout << 1990 << setfill('.') << setw(6) << 135 << endl
+		<< 1991 << setfill('.') << setw(7) << 7290 << endl
+		<< 1992 << setfill('.') << setw(8) << 11300 << endl
+		<< 1993 << setfill('.') << setw(8) << 16200 << endl;
+	return 0;
+}
 
 //lab1.9
 #include <iostream>
