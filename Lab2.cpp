14f264f5d9df2385fc22a1513c48fe4a9795b382
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-03-17 14:31:07 +0300

    Update Lab2.cpp

--- Lab2.cpp
+++ Lab2.cpp
@@ -44,7 +44,33 @@ int main()
 }
 
 //lab2.3
+
+
 //lab2.4
+#include <iostream>
+using namespace std;
+int main()
+{
+	double n1, n2, ans;
+	char oper, ch;
+	do {
+		cout << "\nEnter first number, operator, second number: ";
+		cin >> n1 >> oper >> n2;
+		switch (oper)
+		{
+		case '+': ans = n1 + n2; break;
+		case '-': ans = n1 - n2; break;
+		case '*': ans = n1 * n2; break;
+		case '/': ans = n1 / n2; break;
+		default: ans = 0;
+		}
+		cout << "Answer = " << ans;
+		cout << "\nDo another (Enter 'y' or 'n')? ";
+		cin >> ch;
+	} while (ch != 'n');
+	return 0;
+}
+
 //lab2.5
 //lab2.6
 //lab2.7
