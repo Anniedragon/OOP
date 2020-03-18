16294fb5d85a2461e50706417b10f0fbc40fd630
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-03-18 15:22:11 +0300

    Update Lab2.cpp

--- Lab2.cpp
+++ Lab2.cpp
@@ -142,7 +142,7 @@ int main() {
 #include <iostream>
 using namespace std;
 int main() {
-	float pound1, shil1, pen1, pound2, shil2, pen2, s_pound, s_shil, s_pen;
+	int pound1, shil1, pen1, pound2, shil2, pen2, s_pound, s_shil, s_pen;
 	char flag = 'y';
 	s_pound = 0;
 	s_shil = 0;
@@ -152,15 +152,17 @@ int main() {
 		cin >> pound1 >> shil1 >> pen1;
 		cout << "Enter the second sum: ";
 		cin >> pound2 >> shil2 >> pen2;
-		s_pen = pen1 + pen2;
-		if (s_pen > 12) {
+		s_pen = pen1 + pen2; 
+		while (s_pen > 12) {
 			s_shil += s_pen / 12;
+			s_pen -= 12;
 		}
-		s_shil = shil1 + shil2;
-		if (s_shil > 20) {
+		s_shil += shil1 + shil2;
+		while (s_shil > 20) {
 			s_pound += s_shil / 20;
+			s_shil -= 20;
 		}
-		s_pound = pound1 + pound2;
+		s_pound += pound1 + pound2;
 		cout << s_pound << " " << s_shil << " " << s_pen << endl;
 		cout << "Do you want to continue? (y/n)" << endl;
 		cin >> flag;
