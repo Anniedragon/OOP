ae8ce65066a8600e594e4ec7cd142cb91d6cc661
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-03-20 12:03:49 +0300

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -46,6 +46,31 @@ int main()
 }
 
 //lab3.3
+#include <iostream>
+using namespace std;
+struct Distance
+{
+	int feet;
+	float inches;
+};
+struct Volume
+{
+	Distance length;
+	Distance width;
+	Distance height;
+};
+int main()
+{
+	float l, w, h;
+	Volume room1 = { { 16, 3.5 }, { 12, 6.25 }, { 8, 1.75 } };
+	l = room1.length.feet + room1.length.inches / 12.0;
+	w = room1.width.feet + room1.width.inches / 12.0;
+	h = room1.height.feet + room1.height.inches / 12.0;
+	cout << "Volume = " << l * w * h << " cubic feet\n";
+	return 0;
+}
+
+
 //lab3.4
 //lab3.5
 //lab3.6
