5dfd0b049014a904735f016fe3708bde59195930
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-03-24 15:12:01 +0300

    Update Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 using namespace std;
 float circarea(float radius);
-int main()
-{
+int main() {
 	double rad;
 	setlocale(LC_ALL, "Russian");
 	cout << "\nВведите радиус окружности: ";
@@ -11,13 +10,40 @@ int main()
 	cout << "S площадь равна " << circarea(rad) << endl;
 	return 0;
 }
-float circarea(float r)
-{
+float circarea(float r) {
 	const float PI = 3.14159F;
 	return r * r * PI;
 }
 
 //lab4.2
+#include <iostream>
+using namespace std;
+double power(double n, int p = 2);
+int main() {
+	double number, answer;
+	int pow;
+	char yesorno;
+	setlocale(LC_ALL, "Russian");
+	cout << "\nВведите число: ";
+	cin >> number;
+	cout << "Будете вводить степень (y/n)? ";
+	cin >> yesorno;
+	if (yesorno == 'y') {
+		cout << "Введите степень: ";
+		cin >> pow;
+		answer = power(number, pow);
+}
+	else answer = power(number);
+	cout << "Ответ " << answer << endl;
+	return 0;
+}
+double power(double n, int p) {
+	double result = 1.0;
+	for (int j = 0; j < p; j++)
+		result *= n;
+	return result;
+}
+
 //lab4.3
 //lab4.4
 //lab4.5
