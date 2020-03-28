a2091e25737f4db4825f86cc3172d77a07669c35
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-03-28 17:02:04 +0300

    Update Lab5.cpp

--- Lab5.cpp
+++ Lab5.cpp
@@ -29,6 +29,54 @@ int main() {
 }
 
 //lab5.2
+#include <iostream>
+#include <conio.h>
+using namespace std;
+const char ESC = 27;
+const double TOLL = 0.5;
+class tollBooth
+{
+
+private:
+	unsigned int totalCars; 
+	double totalCash; 
+public: 
+	tollBooth() : totalCars(0), totalCash(0.0){
+	}
+	void payingCar() 
+	{
+		totalCars++; totalCash += TOLL;
+	}
+	void nopayCar() 
+	{
+		totalCars++;
+	}
+	void display()const 
+	{
+		cout << "\n" << "Машины: " << totalCars
+			<< " Деньги: " << totalCash
+			<< endl;
+	}
+};
+int main()
+{
+	tollBooth booth1; 
+	char ch;
+	setlocale(LC_ALL, "Russian");
+	cout << "\nНажмите 0 для машины без оплаты,"
+		<< "\n 1 для каждой оплачивающей машины,"
+		<< "\n Esc для выхода.\n ";
+	do {
+		ch = _getche(); 
+		if (ch == '0') 
+			booth1.nopayCar();
+		if (ch == '1') 
+			booth1.payingCar();
+	} while (ch != ESC); 
+	booth1.display(); 
+	return 0;
+}
+
 //lab5.3
 //lab5.4
 //lab5.5
