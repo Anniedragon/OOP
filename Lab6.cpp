89ba355583d65178848037326e57d79d2a9749e9
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-05-14 19:28:40 +0300

    Update Lab6.cpp

--- Lab6.cpp
+++ Lab6.cpp
@@ -27,6 +27,42 @@ void reversit(char s[])
 }
 
 //lab6.2
+#include <iostream>
+#include <string>
+using namespace std;
+class employee
+{
+private:
+  string name;
+  long number;
+public:
+  void getdata() {
+    cout <<"введите имя: ";cin >>name;
+    cout <<"введите номер: ";cin >>number;
+  }
+  void putdata() {
+    cout <<"\n имя: " <<name;
+    cout <<"\n номер: " <<number;
+  }
+};
+int main() {
+employee emparr [100];
+int n = 0;
+char ch;
+do {
+  cout <<"\nвведите данные о работнике с номером " << n+1;
+  emparr[n++].getdata();
+  cout << "Продолжить (y/n)? ";
+  cin >> ch;
+} while(ch != 'n');
+for (int j = 0; j < n; j++) {
+  cout <<"\nНомер работника " << j + 1;
+  emparr[j].putdata();
+}
+cout <<endl;
+return 0;
+}
+
 //lab6.3
 //lab6.4
 //lab6.5
