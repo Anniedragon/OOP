c02f0cb8bd1d6cf897e6271ec3b535ef38e95077
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-05-26 16:45:09 +0300

    Update Lab7.cpp

--- Lab7.cpp
+++ Lab7.cpp
@@ -209,6 +209,96 @@ int main()
 }
 
 //lab7.5
+#include <iostream>
+using namespace std;
+class time
+{
+private:
+	int hrs, mins, secs;
+public:
+	time() : hrs(0), mins(0), secs(0)
+	{ }
+	time(int h, int m, int s) : hrs(h), mins(m), secs(s)
+	{ }
+	void display()
+	{
+		cout << hrs << ":" << mins << ":" << secs << endl;
+	}
+	time operator + (time t2) {
+		int s = secs + t2.secs;
+		int m = mins + t2.mins;
+		int h = hrs + t2.hrs;
+		if (s > 59)
+		{
+			s -= 60; m++;
+		}
+		if (m > 59)
+		{
+			m -= 60; h++;
+		}
+		return time(h, m, s);
+	}
+	void post_inc(char sign) {
+		if (sign == '+') {
+			hrs++;
+			mins++;
+			secs++;
+			int h = hrs;
+			int m = mins;
+			int s = secs;
+			if (s > 59) {
+				s -= 60; 
+				m++;
+			}
+			if (m > 59)	{
+				m -= 60; 
+				h++;
+			}
+			cout << h << ":" << m << ":" << s << endl;
+		}
+		else if (sign == '-') {
+			int h = hrs--;
+			int m = mins--;
+			int s = secs--;
+			cout << h << ":" << m << ":" << s << endl;
+		}
+	}
+	void pref_inc(char sign) {
+		if (sign == '+') {
+			int h = ++hrs;
+			int m = ++mins;
+			int s = ++secs;
+			if (s > 59) {
+				s -= 60; 
+				m++;
+			}
+			if (m > 59) {
+				m -= 60; 
+				h++;
+			}
+			cout << h << ":" << m << ":" << s << endl;
+		}
+		else if (sign == '-') {
+			int h = --hrs;
+			int m = --mins;
+			int s = --secs;
+			cout << h << ":" << m << ":" << s << endl;
+		}		
+	}
+};
+int main()
+{
+	time time1(5, 59, 59);
+	char sign;
+	cout << "Enter + or - : ";
+	cin >> sign;
+	time1.post_inc(sign);
+	cout << "Enter + or - : ";
+	cin >> sign;
+	time1.pref_inc(sign);
+	return 0;
+}
+
 //lab7.6
 //lab7.7
 //lab7.8
