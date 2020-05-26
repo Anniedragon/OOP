712da6d657d1313787b281225e5a2400d00aefea
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-05-26 18:21:09 +0300

    Update Lab7.cpp

--- Lab7.cpp
+++ Lab7.cpp
@@ -310,6 +310,133 @@ int main()
 }
 
 //lab7.6
+#include <iostream>
+#include <cmath>
+using namespace std;
+class time
+{
+private:
+	int hrs, mins, secs;
+public:
+	time() : hrs(0), mins(0), secs(0)
+	{ }
+	time(int h, int m, int s) : hrs(h), mins(m), secs(s)
+	{ }
+	void display()
+	{
+		cout << hrs << ":" << mins << ":" << secs << endl;
+	}
+	time operator + (time t2) {
+		int s = secs + t2.secs;
+		int m = mins + t2.mins;
+		int h = hrs + t2.hrs;
+		if (s > 59)
+		{
+			s -= 60; m++;
+		}
+		if (m > 59)
+		{
+			m -= 60; h++;
+		}
+		return time(h, m, s);
+	}
+	time operator - (time t2) {
+		int s = abs(secs - t2.secs);
+		int m = abs(mins - t2.mins);
+		int h = abs(hrs - t2.hrs);
+		if (s > 59)
+		{
+			s -= 60; m++;
+		}
+		if (m > 59)
+		{
+			m -= 60; h++;
+		}
+		return time(h, m, s);
+	}
+	time operator * (time t2) {
+		int s = float(secs * t2.secs);
+		int m = float(mins * t2.mins);
+		int h = float(hrs * t2.hrs);
+		if (s > 59)
+		{
+			s -= 60; m++;
+		}
+		if (m > 59)
+		{
+			m -= 60; h++;
+		}
+		return time(h, m, s);
+	}
+	time operator++ () {
+		int s = secs++;
+		int m = mins;
+		int h = hrs;
+		if (s > 59) {
+			s -= 60;
+			m++;
+		}
+		if (m > 59) {
+			m -= 60;
+			h++;
+		}
+		return time(h, m, s);
+	}
+	time operator++ (int i) {
+		int s = secs++;
+		int m = mins;
+		int h = hrs;
+		if (s > 59) {
+			s -= 60;
+			m++;
+		}
+		if (m > 59) {
+			m -= 60;
+			h++;
+		}
+		return time(h, m, s);
+	}
+	time operator-- () {
+		int s = secs--;
+		int m = mins;
+		int h = hrs;
+		if (s > 59) {
+			s -= 60;
+			m++;
+		}
+		if (m > 59) {
+			m -= 60;
+			h++;
+		}
+		return time(h, m, s);
+	}
+	time operator-- (int i) {
+		int s = secs--;
+		int m = mins;
+		int h = hrs;
+		if (s > 59) {
+			s -= 60;
+			m++;
+		}
+		if (m > 59) {
+			m -= 60;
+			h++;
+		}
+		return time(h, m, s);
+	}
+};
+int main()
+{
+	time time1(5, 59, 59);
+	time time2(4, 30, 30);
+	time time3;
+	time3 = time1 - time2;
+	time3.display();
+	time3 = time1 * time2;
+	time3.display();
+	return 0;
+}
+
 //lab7.7
 //lab7.8
 //lab7.9
