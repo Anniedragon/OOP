e832712dc463e359fbdc3207773791323cefd775
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-06-02 17:18:47 +0300

    Update Lab8.cpp

--- Lab8.cpp
+++ Lab8.cpp
@@ -771,4 +771,59 @@ int main() {
 }
 
 //lab8.11
+#include <iostream>
+#include <process.h>
+using namespace std;
+class Stack { 
+protected:              
+	enum { MAX = 3 };    
+	int st[MAX];        
+	int top;                
+public:
+	Stack() { 
+		top = -1; 
+	} 
+	void push(int var) { 
+		st[++top] = var; 
+	}     
+	int pop() { 
+		return st[top--]; 
+	} 
+}; 
+class Stack2 : public Stack {   
+public: 
+	void push(int var) {         
+		if (top >= MAX-1) { 
+			cout << "\nОшибка: стек полон"; 
+			exit(1); 
+		}
+		Stack::push (var);     
+	} 
+	int pop() {         
+		if (top < 0) { 
+			cout << "\nОшибка: стек пуст\n"; 
+			exit (1); 
+		}
+	return Stack::pop();       
+	} 
+};
+
+class pairStack : public Stack2 {
+public:
+	void push(int x, int y) {
+		Stack2::push(x);
+		Stack2::push(y);
+	}
+	void pop() {
+		cout << Stack2::pop() << " " << Stack2::pop() << endl;
+	}
+};
+int main() {   
+	setlocale(LC_ALL, "Russian"); 
+	pairStack p1;
+	p1.push(4, 5);
+	p1.pop();
+	return 0; 
+}
+
 //lab8.12
