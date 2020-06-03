f7c8086de7d9ccc3cd497d4cb94cda3e27ecef7a
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-06-03 14:48:14 +0300

    Update Lab9.cpp

--- Lab9.cpp
+++ Lab9.cpp
@@ -1,4 +1,25 @@
 //lab9.1
+#include <iostream>
+using namespace std;
+int main() {
+	float flarr[100]; 
+	char ch; 
+	int num = 0; 
+	do {
+		cout << "Enter number: "; 
+		cin >> *(flarr + num++); 
+		cout << " Enter another (y/n)? ";
+		cin >> ch;
+	}
+	while (ch != 'n');
+	float total = 0.0; 
+	for (int k = 0; k < num; k++) 
+		total += *(flarr + k);
+	float average = total / num; 
+	cout << "Average is " << average << endl;
+	return 0;
+}
+
 //lab9.2
 //lab9.3
 //lab9.4
