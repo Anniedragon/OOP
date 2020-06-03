cef4bd1a2e5a347236b45911f5f4fe44395174c7
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-06-03 17:34:35 +0300

    Update Lab9.cpp

--- Lab9.cpp
+++ Lab9.cpp
@@ -93,6 +93,56 @@ void order(const char** pp1, const char** pp2) {
 }
 
 //lab9.4
+#include <iostream>
+using namespace std;
+struct link 
+{
+	int data; 
+	link* next; 
+};
+class linklist {
+private:
+	link* first;
+public:
+	linklist() {
+		first = NULL;
+	} 
+	~linklist(); 
+	void additem(int d); 
+	void display(); 
+};
+void linklist::additem(int d) {
+	link* newlink = new link; 
+	newlink->data = d; 
+	newlink->next = first; 
+	first = newlink; 
+}
+void linklist::display() {
+	link* current = first; 
+	while (current != NULL) {
+		cout << endl << current->data; 
+		current = current->next; 
+	}
+}
+linklist::~linklist() {
+	link* current = first; 
+	while (current != NULL) {
+		link* temp = current; 
+		current = current->next; 
+		delete temp; 
+	}
+}
+int main() {
+	linklist li; 
+	li.additem(25); 
+	li.additem(36);
+	li.additem(49);
+	li.additem(64);
+	li.display(); 
+	cout << endl;
+	return 0;
+}
+
 //lab9.5
 //lab9.6
 //lab9.7
