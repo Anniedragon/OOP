42dd3b142add633636b7fd50b73a03fa3111f2da
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-06-04 16:49:48 +0300

    Update Lab9.cpp

--- Lab9.cpp
+++ Lab9.cpp
@@ -197,6 +197,91 @@ int main() {
 }
 
 //lab9.7
+#include <iostream>
+#include <string> 
+using namespace std;
+class person {
+protected:
+	string name;
+	float salary;
+public:
+	void setData() {
+		cout << "Enter name: "; 
+		cin >> name;
+		cout << "Enter salary: "; 
+		cin >> salary;
+	}
+	void printData() {
+		cout << "Name: " << name << endl
+			<< "Salary: " << salary << endl;
+	}
+	string getName() {
+		return name;
+	}
+	float getSalary() {
+		return salary;
+	}
+};
+int main() {
+	void bsort(person**, int);
+	void salsort(person**, int);
+	person* persPtr[100]; 
+	int n = 0; 
+	char choice; 
+	do { 
+		persPtr[n] = new person; 
+		persPtr[n]->setData(); 
+		n++; 
+		cout << "Enter another (y/n)? "; 
+		cin >> choice; 
+	}
+	while (choice == 'y'); 
+	cout << "\nUnsorted list:" << endl;
+	for (int j = 0; j < n; j++) 
+	{
+		persPtr[j]->printData();
+	}
+	bsort(persPtr, n); 
+	cout << "\nSorted name list:" << endl;
+	for (int j = 0; j < n; j++) {
+		persPtr[j]->printData();
+	}
+	cout << endl;
+	salsort(persPtr, n);
+	cout << "\nSorted salary list: " << endl;
+	for (int j = 0; j < n; j++) {
+		persPtr[j]->printData();
+	}
+	cout << endl;
+	return 0;
+} 
+void bsort(person** pp, int n) {
+	void order(person**, person**); 
+	int j, k; 
+	for (j = 0; j < n - 1; j++) 
+		for (k = j + 1; k < n; k++)
+			order(pp + j, pp + k); 
+}
+void order(person** pp1, person** pp2) { 
+	if ((*pp1)->getName() > (*pp2)->getName()) {
+		person* tempptr = *pp1; 
+		*pp1 = *pp2;
+		*pp2 = tempptr;
+	}
+}
+void salsort(person** pp, int n) {
+	int j, k;
+	for (j = 0; j < n - 1; j++) {
+		for (k = j + 1; k < n; k++) {
+			if ((*(pp + j))->getSalary() > (*(pp + k))->getSalary()) {
+				person* tempptr = *(pp + j);
+				*(pp + j) = *(pp + k);
+				*(pp + k) = tempptr;
+			}
+		}
+	}	
+}
+
 //lab9.8
 //lab9.9
 //lab9.10
