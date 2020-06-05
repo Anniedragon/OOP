b8827e053493aae1a2469f7dfb79354ed6333e80
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-06-05 14:56:09 +0300

    Update Lab10.cpp

--- Lab10.cpp
+++ Lab10.cpp
@@ -180,6 +180,35 @@ int main() {
 }
 
 //lab10.4 
+#include <iostream>
+using namespace std;
+class Array {
+private:
+	int* ptr; 
+	int size; 
+public:
+	Array(int s) {
+		size = s; 
+		ptr = new int[s]; 
+	}
+	~Array() {
+		delete[] ptr;
+	}
+	int& operator [] (int j) {
+		return *(ptr + j);
+	}
+};
+int main() {
+	const int ASIZE = 10; 
+	Array arr(ASIZE); 
+	for (int j = 0; j < ASIZE; j++) 
+		arr[j] = j * j;
+	for (int j = 0; j < ASIZE; j++) 
+		cout << arr[j] << ' ';
+	cout << endl;
+	return 0;
+}
+
 //lab10.5
 //lab10.6
 //lab10.7
