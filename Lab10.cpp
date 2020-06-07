50f43322e2e21b31267220b60c4ca7ae51eb0429
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-06-07 13:57:22 +0300

    Update Lab10.cpp

--- Lab10.cpp
+++ Lab10.cpp
@@ -1294,4 +1294,80 @@ int main() {
 }
 
 //lab10.10
+#include <iostream>
+using namespace std;
+struct link
+{
+    int data;
+    link* next;
+};
+class linklist {
+private:
+	link* first;                    
+public:
+	linklist() {
+		first = NULL;
+	}
+	linklist(const linklist& list) {
+		if (list.first == NULL) 
+			return;
+		first = new link();
+		first->data = list.first->data;
+		link* cur1 = first;
+		link* cur2 = list.first;
+		while (cur2->next != NULL) {
+			cur2 = cur2->next;
+			cur1->next = new link();
+			cur1 = cur1->next;
+			cur1->data = cur2->data;
+		}
+	}
+	linklist& operator=(const linklist& list) {
+		*this = linklist(list);
+		return *this;
+	}
+	~linklist();                    
+	void additem(int d);            
+	void display();                 
+};
+void linklist::additem(int d) {
+	link* newlink = new link;          
+	newlink->data = d;                 
+	newlink->next = first;             
+	first = newlink;                   
+}
+void linklist::display() {
+	link* current = first;             
+	while (current != NULL) {
+		cout << " " << current->data;  
+		current = current->next;        
+	}
+}
+linklist::~linklist() {
+	link* current = first;             
+	cout << "\nList was deleted!\n";
+	while (current != NULL)           
+	{
+		link* temp = current;           
+		current = current->next;        
+		cout << temp->data << " ";
+		delete temp;                    
+	}
+}
+int main() {
+	linklist* li = new linklist();
+	cout << "\nNew list:\n";
+	li->additem(25);    
+	li->additem(36);
+	li->additem(49);
+	li->additem(64);
+	li->display();      
+	linklist copy = *li;
+	delete li;
+	cout << "\nCopy of the list:\n";
+	copy.display();
+
+    return 0;
+}
+
 //lab10.11
