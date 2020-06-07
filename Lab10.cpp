a76e87a8fd8ead81ba14d2be10e2d338c2458ce6
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-06-07 17:04:19 +0300

    Update Lab10.cpp

--- Lab10.cpp
+++ Lab10.cpp
@@ -1371,3 +1371,552 @@ int main() {
 }
 
 //lab10.11
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+using namespace std;
+const int LEN = 80;
+const int MAX = 40;
+class bMoney {
+private:
+	long double money;
+	string sum;
+public:
+	bMoney() {
+		money = 0;
+	}
+	bMoney(string u_sum) {
+		money = mstold(u_sum);
+	}
+	bMoney(long double d) {
+		money = d;
+	}
+	void getmoney() {
+		string u_money;
+		cout << "Enter a money string: ";
+		cin >> u_money;
+		money = mstold(u_money);
+	}
+	long double mstold(string u_sum) {
+		sum = u_sum;
+		string s;
+		int len;
+		len = sum.size() - 3;
+		for (int i = 0; i < len; i++) {
+			if ((sum[i] != '$') && (sum[i] != ',')) {
+				if ((sum[i] == '.') && (i + 2 == len - 1)) {
+					s += sum[i];
+				}
+				else s += sum[i];
+			}
+		}
+		len = s.size();
+		long double pow = len - 1;
+		long double result = 0;
+		int temp = 0;
+		for (int i = 0; i < len; i++) {
+			switch (s[i]) {
+			case '1':
+				temp = 1;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '2':
+				temp = temp * 2;
+				for (int j = 0; j < pow; j++) {
+					temp = temp * 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '3':
+				temp = 3;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '4':
+				temp = 4;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '5':
+				temp = 5;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '6':
+				temp = 6;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '7':
+				temp = 7;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '8':
+				temp = 8;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			case '9':
+				temp = 9;
+				for (int j = 0; j < pow; j++) {
+					temp *= 10;
+				}
+				result += temp;
+				temp = 1;
+				break;
+			default:
+				break;
+			}
+			pow--;
+		}
+		long double result1 = 0;
+		pow = 0.1;
+		for (int i = s.size() - 2; i <= s.size() - 1; i++) {
+			switch (s[i]) {
+			case '1':
+				temp = 1;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '2':
+				temp = 2;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '3':
+				temp = 3;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '4':
+				temp = 4;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '5':
+				temp = 5;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '6':
+				temp = 6;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '7':
+				temp = 7;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '8':
+				temp = 8;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			case '9':
+				temp = 9;
+				result1 += temp * pow;
+				temp = 1;
+				break;
+			}
+			pow /= 10;
+		}
+		result += result1;
+		money = result;
+		return money;
+	}
+	void ldtoms(long double sum) {
+		string ustring, sum_string;
+		char temp;
+		long double cel, drob;
+		int ch = 0;
+		drob = modf(sum, &cel);
+		int sum1 = cel;
+		int count = 0;
+		while (sum1 != 0) {
+			sum1 /= 10;
+			count++;
+		}
+		int cel_int = int(cel);
+		for (int i = 0; i < count; i++) {
+			ch = cel_int % 10;
+			switch (ch) {
+			case 1:
+				ustring += '1';
+				break;
+			case 2:
+				ustring += '2';
+				break;
+			case 3:
+				ustring += '3';
+				break;
+			case 4:
+				ustring += '4';
+				break;
+			case 5:
+				ustring += '5';
+				break;
+			case 6:
+				ustring += '6';
+				break;
+			case 7:
+				ustring += '7';
+				break;
+			case 8:
+				ustring += '8';
+				break;
+			case 9:
+				ustring += '9';
+				break;
+			case 0:
+				ustring += '0';
+				break;
+			}
+			cel_int /= 10;
+		}
+		for (int i = 0; i < count / 2; i++) {
+			temp = ustring[i];
+			ustring[i] = ustring[count - i - 1];
+			ustring[count - i - 1] = temp;
+		}
+		ustring += '.';
+		drob *= 10;
+		long double drob_cel, drob_drob;
+		drob_drob = modf(drob, &drob_cel);
+		int drob_cel_int = int(drob_cel);
+		drob_drob *= 10;
+		int drob_drob_int = int(drob_drob);
+		switch (drob_cel_int) {
+		case 1:
+			ustring += '1';
+			break;
+		case 2:
+			ustring += '2';
+			break;
+		case 3:
+			ustring += '3';
+			break;
+		case 4:
+			ustring += '4';
+			break;
+		case 5:
+			ustring += '5';
+			break;
+		case 6:
+			ustring += '6';
+			break;
+		case 7:
+			ustring += '7';
+			break;
+		case 8:
+			ustring += '8';
+			break;
+		case 9:
+			ustring += '9';
+			break;
+		case 0:
+			ustring += '0';
+			break;
+		}
+		switch (drob_drob_int) {
+		case 1:
+			ustring += '1';
+			break;
+		case 2:
+			ustring += '2';
+			break;
+		case 3:
+			ustring += '3';
+			break;
+		case 4:
+			ustring += '4';
+			break;
+		case 5:
+			ustring += '5';
+			break;
+		case 6:
+			ustring += '6';
+			break;
+		case 7:
+			ustring += '7';
+			break;
+		case 8:
+			ustring += '8';
+			break;
+		case 9:
+			ustring += '9';
+			break;
+		case 0:
+			ustring += '0';
+			break;
+		}
+		sum_string += '$';
+		int k = 0;
+		for (int i = 0; i < count + 3; i++) {
+			if (ustring[i + 1] == '.') {
+				sum_string += ustring[i];
+				k = 0;
+			}
+			else sum_string += ustring[i];
+			k++;
+			if ((k == 3) && (ustring[i + 1] != '.') && (i < count)) {
+				sum_string += ',';
+				k = 0;
+			}
+		}
+		sum_string[count + 5] = '\0';
+		cout << sum_string << endl;
+	}
+	bMoney operator + (bMoney b2) {
+		money = money + b2.money;
+		return money;
+	}
+	bMoney operator - (bMoney b2) {
+		money = abs(money - b2.money);
+		return money;
+	}
+	bMoney operator * (long double b2) {
+		money = money * b2;
+		return money;
+	}
+	bMoney operator / (bMoney b2) {
+		money = money / b2.money;
+		return money;
+	}
+	bMoney operator / (long double b2) {
+		money = money / b2;
+		return money;
+	}
+	friend bMoney operator * (long double m1, bMoney b2) {
+		return bMoney(m1 * b2.money);
+	}
+	friend bMoney operator / (long double m1, bMoney b2) {
+		return bMoney(m1 / b2.money);
+	}
+	void putmoney() {
+		cout << money << endl;
+	}
+	friend bMoney bMround(bMoney b) {
+		long double drob, cel;
+		drob = modf(b.money, &cel);
+		if (drob < 0.5) {
+			drob = 0;
+		}
+		else drob = 1;
+		return bMoney(cel + drob);
+	}
+};
+class token {
+public:
+	virtual float getnumber() = 0;
+	virtual char getoperator() = 0;
+};
+class Operator : public token {
+private:
+	char oper;
+public:
+	Operator(char ch) {
+		oper = ch;
+	}
+	char getoperator() {
+		return oper;
+	}
+	float getnumber() { 
+		return 0; 
+	}
+};
+class number : public token {
+private:
+	float fnum;
+public:
+	number(float f) {
+		fnum = f;
+	}
+	float getnumber() {
+		return fnum;
+	}
+	char getoperator() { 
+		return 0; 
+	}
+};
+class Stack {
+private:
+	token* st[100];
+	int top;
+public:
+	Stack() {
+		top = 0;
+	}
+	void push(float var) {
+		st[++top] = new number(var);
+	}
+	void push(char var) {
+		st[++top] = new Operator(var);
+	}
+	float pop(int useless) {
+		return st[top--]->getnumber();
+	}
+	char pop(char useless) {
+		return st[top--]->getoperator();
+	}
+	int gettop() {
+		return top;
+	}
+};
+class express {
+private:
+	Stack s; 
+	char* pStr; 
+	int len; 
+public:
+	express(char* ptr) {
+		pStr = ptr; 
+		len = strlen(pStr); 
+	}
+	void parse(); 
+	float solve(); 
+};
+void express::parse() {
+	int lock = 0;
+	float m2;
+	char ch;
+	float chisl = 0;
+	float lastval;
+	char lastop;
+	for (int j = 0; j < len; j++)
+	{
+		ch = pStr[j];
+		if ((ch >= '0' && ch <= '9') || (ch == '.')) {
+			if (ch >= '0' && ch <= '9' && lock == 0) {
+				chisl += (float)ch - 48;
+				chisl *= 10;
+			}
+			if (ch == '.') {
+				chisl /= 10;
+				lock = 1;
+			}
+			if (ch >= '0' && ch <= '9' && lock != 0) {
+				m2 = (float)ch - 48;
+				for (int k = 0; k < lock; k++) m2 /= 10;
+				lock++;
+				chisl += m2;
+				m2 = 0;
+			}
+			if ((pStr[j + 1] < '0' || pStr[j + 1] > '9') && (pStr[j + 1] != '.')) {
+				s.push(chisl);
+				chisl = 0;
+				lock = 0;
+			}
+		}
+		else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+			if (s.gettop() == 1)
+				s.push(ch);
+			else {
+				lastval = s.pop(1);
+				lastop = s.pop('1');
+
+				if ((ch == '*' || ch == '/') &&
+					(lastop == '+' || lastop == '-'))
+				{
+					s.push(lastop);
+					s.push(lastval);
+				}
+				else {
+					switch (lastop) {
+					case '+': 
+						s.push(s.pop(1) + lastval); 
+						break;
+					case '-': 
+						s.push(s.pop(1) - lastval); 
+						break;
+					case '*': 
+						s.push(s.pop(1) * lastval); 
+						break;
+					case '/': 
+						s.push(s.pop(1) / lastval); 
+						break;
+					default: 
+						cout << "\nUnknown oper"; 
+						exit(1);
+					}
+				}
+				s.push(ch);
+			}
+		}
+		else {
+			cout << "\nUnknown input character"; 
+			exit(1);
+		}
+	}
+}
+float express::solve() {
+	float lastval;
+	while (s.gettop() > 1)
+	{
+		lastval = s.pop(1);
+		switch ((char)s.pop('1'))
+		{
+		case '+': 
+			s.push(s.pop(1) + lastval); 
+			break;
+		case '-': 
+			s.push(s.pop(1) - lastval); 
+			break;
+		case '*': 
+			s.push(s.pop(1) * lastval); 
+			break;
+		case '/': 
+			s.push(s.pop(1) / lastval); 
+			break;
+		default: 
+			cout << "\nUnknown operator"; 
+			exit(1);
+		}
+	}
+	return float(s.pop(1));
+}
+int main() {
+	bMoney* ar[100];
+	char ans; 
+	char string[LEN]; 
+	cout << "\nEnter an arithmetic expression of the form 2+3*4/3-2." << endl
+		<< "No number may have more than one digit." << endl	
+		<< "Don't use any spaces or parentheses." << endl;
+	do {
+		cout << "\nEnter expresssion: ";
+		cin >> string; 
+		string[strlen(string)] = '\0';
+		express* eptr = new express(string); 
+		eptr->parse(); 
+		cout << "\nThe numerical value is: " << eptr->solve(); 
+		delete eptr; 
+		cout << "\nDo another (Enter y or n)? ";
+		cin >> ans;
+	} while (ans == 'y');
+	return 0;
+}
